1e320bc287c5bfdc0138db68e9881159c25d6fee
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-03-06 00:30:53 +1100

    Update project.js giving every Project a unique ID and a prototype method to access their ID and a new method for the ProjectAggregator to get a new unique ID. Get Project now uses unique ID instead of indexes.

--- src/project.js
+++ src/project.js
@@ -5,26 +5,35 @@ const Todo = name =>
     return {name, description, dueDate, priority, complete};
 }
 
-const Project = name =>
+const Project = (id, name) =>
 {
-    return {name, description: '', todos: []};
+    const proto = {
+        getId: () => {return id},
+    }
+
+    return Object.assign(Object.create(proto), {name, description: '', todos: []});
 }
 
 const ProjectAggregator = (() => {
     const _projects = [];
+    let uniqueId = 0;
+
+    const getNewUniqueId = () =>
+    {
+        return uniqueId++;
+    }
 
     const addProject = name =>
     {
-        _projects.push(Project(name));
+        _projects.push(Project(getNewUniqueId(), name));
         console.log(_projects);
         EventAggregator.publish('project created', _projects[_projects.length-1].name);
     }
     EventAggregator.subscribe('add project', name => {addProject(name);});
 
-    const getProject = index =>
+    const getProject = id =>
     {
-        console.log(_project[index]);
-        return _projects[index];
+        return _projects.find(project => {project.getId() == id});
     }
 
     return {addProject, getProject}
