f1b1e4e8e13ddb6b51975bb01c2fe9d8d56eb7ca
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-03-08 20:30:36 +1100

    Insert function to check if localStorage is avaliable. Add function to initialise webapp to index.js.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import DisplayController from './displayController.js';
 import EventAggregator from './event.js';
+import { storageAvailable } from './storage.js';
 import './style.css';
 
-DisplayController.initHTML();
+if(storageAvailable('localStorage'))
+{
 
-EventAggregator.publish('add project', 'Default');
-EventAggregator.publish('add project', 'Trip to Rome');
-EventAggregator.publish('add project', 'Todo list project');
-EventAggregator.publish('select project', 0);
+}
+else
+{
+    init();
+}
 
-EventAggregator.publish('update project', 1, {name: 'New Default', description: "This is the default todo list if you have no other todo lists."});
+function init()
+{
+    DisplayController.initHTML();
 
+    EventAggregator.publish('add project', 'Default');
+    EventAggregator.publish('select project', 0);
+}
